1a329a0b591a2ff1462c8aab20ca0d0e5c794fd5
MadEldar1 <markskilink@gmail.com>
2021-03-23 13:21:21 +0700

    Async Prime Finder

---
+++ Homework_2021_03_23/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Homework_2021_03_23
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            PrintIntList(await PrimeFinder.FindInRange(1, 100));
+        }
+
+        public static void PrintIntList(List<int> list) {
+            foreach (int i in list) {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
